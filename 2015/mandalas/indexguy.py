b7984bfbbca5c24c9269a3e252177eb053dd5e98
benmca <ben@listenfaster.com>
2017-03-07 22:12:43 -0800

    Updated to match thuja lib.

--- 2015/mandalas/indexguy.py
+++ 2015/mandalas/indexguy.py
@@ -1,54 +1,67 @@
-import composition
-import composition.itemstream as ci
-import composition.score as cs
+from thuja.itemstream import Itemstream
+from thuja.generator import Generator
+from thuja.generator import keys
+import thuja.utils as utils
+import thuja.csound_utils as cs_utils
+
+from collections import OrderedDict
 import numpy as np
-
-# rhythms = ci.itemstream(['e'], 'sequence', tempo=np.linspace(60, 500, 64).tolist() + np.linspace(500, 60, 64).tolist(),
-rhythms = ci.itemstream(['q', 'q', 's', 'e', 's', 's', 'e', 's', 'q', 'e.', 'e', 'q', 'e', 's'], 'sequence', tempo=80,
-                        notetype='rhythm')
-dur =
-amps = ci.itemstream([3])
-pitches = ci.itemstream([1], notetype='number')
-# pitches.streammode = 'heap'
-pan = ci.itemstream([10])
-dist = ci.itemstream([10])
-pct = ci.itemstream([.01])
-
-indx = ci.itemstream([.018, .697, 1.376, 1.538, 1.869, 2.032, 2.2, 2.543, 2.705, 3.373, 3.895, 4.232, 4.894, 5.236],
-                     'heap', notetype='number')
-orig_rhythms = ci.itemstream(['q', 'q', 's', 'e', 's', 's', 'e', 's', 'q', 'e.', 'e', 'q', 'e', 's'], 'sequence',
-                             tempo=120, notetype='rhythm')
+import csnd6
 
 
-s = cs.score(rhythms, [amps, pitches, pan, dist, pct, indx, orig_rhythms], note_limit=300)
-s.gen_lines = [';sine\n', 'f 1 0 16384 10 1\n', ';saw', 'f 2 0 256 7 0 128 1 0 -1 128 0\n', ';pulse\n',
+
+def post_processs(note):
+    item = g.context['tuplestream'].get_next_value()
+    indx = g.context['indexes'].index(item[keys.index])
+    orig_rhythm = g.context['orig_rhythms'][indx]
+    note.rhythm = utils.rhythm_to_duration(item[keys.rhythm], g.context['tuplestream'].tempo)
+    note.pfields[keys.index] = item[keys.index]
+    note.pfields['orig_rhythm'] = utils.rhythm_to_duration(orig_rhythm, g.context['tuplestream'].tempo)
+    note.pfields[keys.frequency] = g.context['tuplestream'].tempo / utils.quarter_duration_to_tempo(.697-.018)
+    pass
+
+g = Generator(
+    streams=OrderedDict([
+        (keys.instrument, Itemstream([1])),
+        (keys.duration, lambda note:note.pfields['orig_rhythm']),
+        (keys.amplitude, Itemstream([3])),
+        (keys.frequency, Itemstream([1], notetype='number')),
+        (keys.pan, Itemstream([45])),
+        (keys.distance, Itemstream([10])),
+        (keys.percent, Itemstream([.01]))
+    ]),
+    pfields=[
+        keys.instrument,
+        keys.start_time,
+        keys.duration,
+        keys.amplitude,
+        keys.frequency,
+        keys.pan,
+        keys.distance,
+        keys.percent,
+        keys.index,
+        'orig_rhythm'
+    ],
+    note_limit=300,
+    post_processes=[post_processs]
+)
+g.context['rhythms'] = ['q', 'q', 's', 'e', 's', 's', 'e', 's', 'q', 'e.', 'e', 'q', 'e', 's']
+g.context['indexes'] = [.018, .697, 1.376, 1.538, 1.869, 2.032, 2.2, 2.543, 2.705, 3.373, 3.895, 4.232, 4.894, 5.236]
+g.context['orig_rhythms'] = ['q', 'q', 's', 'e', 's', 's', 'e', 's', 'q', 'e.', 'e', 'q', 'e', 's']
+g.context['tuplestream'] = Itemstream(mapping_keys=[keys.rhythm, keys.index],
+                                      mapping_lists=[g.context['rhythms'],
+                                                     g.context['indexes']],
+                                      tempo=160)
+
+g.gen_lines = [';sine\n',
+               'f 1 0 16384 10 1\n',
+               ';saw',
+               'f 2 0 256 7 0 128 1 0 -1 128 0\n',
+               ';pulse\n',
                'f 3 0 256 7 1 128 1 0 -1 128 -1\n']
-s.durstream = ci.itemstream([.1])
-s.instr = 1
-s.generate_notes()
+g.generate_notes()
 
-s.streams[2] = ci.itemstream([45])
-s.generate_notes()
 
-# s.rhythmstream.tempo = np.linspace(60, 500, 64).tolist() + np.linspace(500, 60, 64).tolist()
-# s.streams[1] = ci.itemstream(['d4'] * 7 + ['d2'], notetype='pitch')
-# s.streams[2] = ci.itemstream([45])
-# s.note_limit = 330
-# s.generate_notes()
+g.end_lines = ['i99 0 ' + str(g.score_dur+10) + '\n']
 
-# s.rhythmstream.tempo = np.linspace(30, 300, 64).tolist() + np.linspace(300, 30, 64).tolist()
-# s.streams[1] = ci.itemstream(['e4'] * 7 + ['e2'], notetype='pitch')
-# s.streams[2] = ci.itemstream([90])
-# s.note_limit = 154
-# s.generate_notes()
-
-output = ""
-for x in range(len(s.gen_lines)):
-    output += s.gen_lines[x]
-for x in range(len(s.notes)):
-    output += s.notes[x]
-
-s.end_lines = ['i99 0 ' + str(s.score_dur) + '\n']
-
-s.generate_score("indexguy.sco")
-# score  = s.generate_score_string()
+cs_utils.play_csound("indexguy.orc", g)
--- 2015/mandalas/tempo_canon.py
+++ 2015/mandalas/tempo_canon.py
@@ -1,42 +1,55 @@
-
-import composition
-import composition.itemstream as ci
-import composition.score as cs
+from thuja.itemstream import Itemstream
+from thuja.generator import Generator
+from thuja.generator import keys
+import thuja.utils as utils
+import thuja.csound_utils as cs_utils
 import numpy as np
+from collections import OrderedDict
+import copy
 
-rhythms = ci.itemstream(['e'],'sequence', tempo=np.linspace(60,500,64).tolist()+np.linspace(500,60,64).tolist(), notetype='rhythm')
-amps = ci.itemstream([3])
-pitches = ci.itemstream(['c4']*7+['c2'], notetype='pitch')
-#pitches.streammode = 'heap'
-pan = ci.itemstream([0])
-dist= ci.itemstream([10])
-pct = ci.itemstream([.01])
+rhythms = Itemstream(['e'],'sequence', tempo=np.linspace(60,80,32).tolist()+np.linspace(80,60,32).tolist(), notetype='rhythm')
 
-s = cs.score(rhythms,[amps,pitches,pan,dist,pct], note_limit=300)
-s.gen_lines = [';sine\n','f 1 0 16384 10 1\n',';saw','f 2 0 256 7 0 128 1 0 -1 128 0\n',';pulse\n','f 3 0 256 7 1 128 1 0 -1 128 -1\n']
-s.durstream = ci.itemstream([.1])
-s.instr = 1
-s.generate_notes()
+g = Generator(
+    streams=OrderedDict([
+        (keys.instrument, Itemstream([1])),
+        (keys.rhythm, rhythms),
+        (keys.duration, Itemstream([.1])),
+        (keys.amplitude, Itemstream([3])),
+        (keys.frequency, Itemstream(['b4'], notetype='pitch',streammode = 'heap')),
+        (keys.pan, Itemstream([0])),
+        (keys.distance, Itemstream([10])),
+        (keys.percent, Itemstream([.01]))
+    ]),
+    note_limit=300
+)
+g.gen_lines = [';sine\n','f 1 0 16384 10 1\n',';saw','f 2 0 256 7 0 128 1 0 -1 128 0\n',';pulse\n','f 3 0 256 7 1 128 1 0 -1 128 -1\n']
 
-s.rhythmstream.tempo = np.linspace(60,500,64).tolist()+np.linspace(500,60,64).tolist()
-s.streams[1] = ci.itemstream(['d4']*7+['d2'], notetype='pitch')
-s.streams[2] = ci.itemstream([45])
-s.note_limit=330
-s.generate_notes()
 
-s.rhythmstream.tempo = np.linspace(30,300,64).tolist()+np.linspace(300,30,64).tolist()
-s.streams[1] = ci.itemstream(['e4']*7+['e2'], notetype='pitch')
-s.streams[2] = ci.itemstream([90])
-s.note_limit=154
-s.generate_notes()
+g2 = copy.deepcopy(g)
+g2.streams[keys.rhythm].tempo = np.linspace(60,100,32).tolist()+np.linspace(100,60,32).tolist()
+g2.streams[keys.frequency] = Itemstream(['c4'], notetype='pitch')
+g2.streams[keys.pan] = Itemstream([30])
+g2.note_limit=330
+g.add_generator(g2)
 
-output = ""
-for x in range(len(s.gen_lines)):
-    output += s.gen_lines[x]
-for x in range(len(s.notes)):
-    output += s.notes[x]
+g3 = copy.deepcopy(g)
+g3.streams[keys.rhythm].tempo = np.linspace(60,120,32).tolist()+np.linspace(120,60,32).tolist()
+g3.streams[keys.frequency] = Itemstream(['d4'], notetype='pitch')
+g3.streams[keys.pan] = Itemstream([60])
+g3.note_limit=154
 
-s.end_lines = ['i99 0 ' + str(s.score_dur) + '\n']
+g.add_generator(g3)
 
-s.generate_score("tempo_canon.sco")
-#score  = s.generate_score_string()
+g4 = copy.deepcopy(g)
+g4.streams[keys.rhythm].tempo = np.linspace(60,140,32).tolist()+np.linspace(140,60,32).tolist()
+g4.streams[keys.frequency] = Itemstream(['e4'], notetype='pitch')
+g4.streams[keys.pan] = Itemstream([90])
+g4.note_limit=154
+
+g.generate_notes()
+
+g.end_lines = ['i99 0 ' + str(g.score_dur) + '\n']
+
+# g.generate_score("tempo_canon.sco")
+# print(g.generate_score_string())
+cs_utils.play_csound('../sine.orc', g)
