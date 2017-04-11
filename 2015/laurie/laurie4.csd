9974d5afcb2ad7d2901b1809b8be11135a877ed1
benmca <ben@listenfaster.com>
2017-04-11 08:20:07 -0700

    updated indexguy  to thuja

--- 2015/laurie/laurie4.csd
+++ 2015/laurie/laurie4.csd
@@ -1,19 +1,64 @@
 <CsoundSynthesizer>
 <CsOptions>
--odac0 -b128 -B1024  -M0 -Q0 -+rtmidi=virtual
+-odac0 -b128 -B1024  -M0
 </CsOptions>
 <CsInstruments>
-
 sr=44100
-kr=4410
-ksmps=10
+kr=441
 nchnls=1
+
 pyinit
 
 pyruni {{
+import random
+from thuja.itemstream import Itemstream
 
+random.seed()
+class PitchQueue:
+    curdx = 0;
+    rhythm_stream = Itemstream(['s','s','s', 's', '32', '32', '32', '32'],'sequence', tempo=60)
+    rhythm_stream.notetype = 'rhythm'
+
+    def __init__(self):
+        self.items = []
+        self.max_size = 8
+
+    def isEmpty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        if len(self.items) >= self.max_size:
+            self.dequeue()
+        self.items.append(item)
+        print 'enqueue: ' + str(item) + ', items: ' + str(self.items)
+
+    def dequeue(self):
+        return self.items.pop(0)
+
+    def size(self):
+        return len(self.items)
+
+    def get_random_pitch(self, arg):
+        x = 0.0
+        if len(self.items) > 1:
+            x = self.items[random.randint(0, len(self.items) - 1)]
+        return x
+    def get_next_pitch(self, arg):
+        x = 0.0
+        if len(self.items) > 0:
+            indx = self.curdx % len(self.items)
+            x = self.items[indx]
+        self.curdx = self.curdx + 1
+        return x
+    def get_next_dur(self, arg):
+        dur = self.rhythm_stream.get_next_value()
+        return dur
+    def reverse(self,arg):
+        self.items.reverse()
+
+
+pitches = PitchQueue()
 }}
-
 
 
 instr 1
@@ -28,18 +73,20 @@ ktrig = 0
 contin:
 endin
 
+
 instr 2
 ktrig init 1
 idur = .25
 iamp = 10000
 if (ktrig == 0) goto contin
-kfreq   pycall1     "pitches.get_random_pitch", 0
+kfreq   pycall1     "pitches.get_next_pitch", 0
 kdur   pycall1     "pitches.get_next_dur", 0
 		event 		"i", 10, 0, .25, iamp, kfreq
 		event 		"i", 2, kdur, .25
 ktrig = 0
 contin:
 endin
+
 
 instr 10
 inote = p5
--- 2015/mandalas/indexguy.py
+++ 2015/mandalas/indexguy.py
@@ -1,24 +1,27 @@
 from thuja.itemstream import Itemstream
 from thuja.generator import Generator
 from thuja.generator import keys
+from thuja.itemstream import streammodes
 import thuja.utils as utils
 import thuja.csound_utils as cs_utils
 
 from collections import OrderedDict
 import numpy as np
 import csnd6
+import copy
 
 
 
-def post_processs(note):
-    item = g.context['tuplestream'].get_next_value()
-    indx = g.context['indexes'].index(item[keys.index])
-    orig_rhythm = g.context['orig_rhythms'][indx]
-    note.rhythm = utils.rhythm_to_duration(item[keys.rhythm], g.context['tuplestream'].tempo)
+def post_process(note, context):
+    item = context['tuplestream'].get_next_value()
+    indx = context['indexes'].index(item[keys.index])
+    orig_rhythm = context['orig_rhythms'][indx]
+    note.rhythm = utils.rhythm_to_duration(item[keys.rhythm], context['tuplestream'].tempo)
     note.pfields[keys.index] = item[keys.index]
-    note.pfields['orig_rhythm'] = utils.rhythm_to_duration(orig_rhythm, g.context['tuplestream'].tempo)
-    note.pfields[keys.frequency] = g.context['tuplestream'].tempo / utils.quarter_duration_to_tempo(.697-.018)
+    note.pfields['orig_rhythm'] = utils.rhythm_to_duration(orig_rhythm, context['tuplestream'].tempo)
+    note.pfields[keys.frequency] = context['tuplestream'].tempo / utils.quarter_duration_to_tempo(.697-.018)
     pass
+
 
 g = Generator(
     streams=OrderedDict([
@@ -43,7 +46,7 @@ g = Generator(
         'orig_rhythm'
     ],
     note_limit=300,
-    post_processes=[post_processs]
+    post_processes=[post_process]
 )
 g.context['rhythms'] = ['q', 'q', 's', 'e', 's', 's', 'e', 's', 'q', 'e.', 'e', 'q', 'e', 's']
 g.context['indexes'] = [.018, .697, 1.376, 1.538, 1.869, 2.032, 2.2, 2.543, 2.705, 3.373, 3.895, 4.232, 4.894, 5.236]
@@ -51,7 +54,26 @@ g.context['orig_rhythms'] = ['q', 'q', 's', 'e', 's', 's', 'e', 's', 'q', 'e.', 
 g.context['tuplestream'] = Itemstream(mapping_keys=[keys.rhythm, keys.index],
                                       mapping_lists=[g.context['rhythms'],
                                                      g.context['indexes']],
-                                      tempo=160)
+                                      tempo=80,
+                                      streammode=streammodes.random)
+
+g2 = copy.deepcopy(g)
+g2.context['indexes'] = [.018]
+g2.context['rhythms'] = ['q']
+g2.streams[keys.pan] = Itemstream([0])
+g2.context['tuplestream'] = Itemstream(mapping_keys=[keys.rhythm, keys.index],
+                                      mapping_lists=[g2.context['rhythms'],
+                                                     g2.context['indexes']],
+                                      tempo=80)
+
+g3 = copy.deepcopy(g2)
+g3.context['indexes'] = [.018]
+g3.context['rhythms'] = ['s']
+g3.streams[keys.pan] = Itemstream([90])
+g3.context['tuplestream'] = Itemstream(mapping_keys=[keys.rhythm, keys.index],
+                                      mapping_lists=[g3.context['rhythms'],
+                                                     g3.context['indexes']],
+                                      tempo=80)
 
 g.gen_lines = [';sine\n',
                'f 1 0 16384 10 1\n',
@@ -59,7 +81,11 @@ g.gen_lines = [';sine\n',
                'f 2 0 256 7 0 128 1 0 -1 128 0\n',
                ';pulse\n',
                'f 3 0 256 7 1 128 1 0 -1 128 -1\n']
+
+g.add_generator(g2)
+g.add_generator(g3)
 g.generate_notes()
+
 
 
 g.end_lines = ['i99 0 ' + str(g.score_dur+10) + '\n']
