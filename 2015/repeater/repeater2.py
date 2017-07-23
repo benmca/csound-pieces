d6b1b036ca42b5dc6e89b4e063a7df170c42adb4
benmca <ben@listenfaster.com>
2017-07-23 14:22:47 -0700

    Converted items to Thuja - generator

--- 2015/repeater/repeater2.py
+++ 2015/repeater/repeater2.py
@@ -1,37 +1,36 @@
-# use variable blueDuration for duration from blue
-# use variable userConfigDir for user's .blue dir
-# use variable blueLibDir for blue's lib directory
-# use variable blueProjectDir for this project's directory
-# use variable score at end of script to bring score back into blue
 from thuja.itemstream import Itemstream
-from thuja.score import Score
+from thuja.generator import Generator
+from thuja.generator import keys
+from thuja.itemstream import streammodes
+import thuja.utils as utils
+import thuja.csound_utils as cs_utils
+
+from collections import OrderedDict
+import numpy as np
+import csnd6
+import copy
+
 import numpy.random as nprnd
 
-rhythms = Itemstream([.05*nprnd.random() for i in xrange(40)], 'sequence', tempo=60)
-# rhythms = Itemstream([.1*nprnd.random() for i in xrange(10)], 'sequence', tempo=60)
-# rhythms = Itemstream([.5+nprnd.random() for i in xrange(10)], 'sequence', tempo=60)
-# rhythms = Itemstream([1+nprnd.random() for i in xrange(10)], 'sequence', tempo=60)
-# rhythms.notetype = 'rhythm'
-amps = Itemstream([.5])
+g = Generator(
+    streams=OrderedDict([
+        (keys.instrument, Itemstream([1])),
+        (keys.rhythm,  Itemstream([.05*nprnd.random() for i in xrange(40)], 'sequence', tempo=60)),
+        (keys.duration, Itemstream([(2*60)+44.859])),
+        (keys.amplitude, Itemstream([.5])),
+        (keys.frequency, Itemstream([1])),
+        (keys.pan, Itemstream([45])),
+        (keys.distance, Itemstream([10])),
+        (keys.percent, Itemstream([.1]))
+    ]),
+    note_limit=40
+)
 
-pitches = Itemstream([1])
-# pitches.streammode = 'heap'
 
-pan = Itemstream([45])
-dist = Itemstream([10])
-pct = Itemstream([.1])
-
-s = Score(rhythms, [amps, pitches, pan, dist, pct], note_limit=40)
-
-s.gen_lines = [
+g.gen_lines = [
     "f 1 0 16384 10 1\nf 2 0 256 7 0 128 1 0 -1 128 0\nf 3 0 256 7 1 128 1 0 -1 128 -1\nf4 0 131072 1 \"/Users/benmca/Music/Portfolio/snd/ShortwaveSounds/TimeSignals/OnTheHour_Distorted.wav\"\nf5 0 16384 9 .5 1 0\n"]
-s.durstream = Itemstream([(2*60)+44.859])
-s.instr = Itemstream([1])
-s.generate_notes()
+g.generate_notes()
+g.end_lines = ['i99 0 ' + str(g.score_dur) + '\n']
 
-s.end_lines = ['i99 0 ' + str(s.score_dur) + '\n']
-
-# s.generate_score("repeater2-.05.sco")
-cs_utils.play_csound("indexguy.orc", g)
-# print(s.generate_score_string())
+cs_utils.play_csound("repeater.orc", g)
 
