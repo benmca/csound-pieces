7a0bbd70caa742b51a8d57efaec019c4272ff810
benmca <bmcallister@xpansiv.com>
2019-11-22 12:23:08 -0800

    working through 3.0 interface

--- thuja-ep/books-style/1.py
+++ thuja-ep/books-style/1.py
@@ -6,9 +6,6 @@ from thuja.itemstream import streammodes
 from thuja.itemstream import notetypes
 import thuja.utils as utils
 import thuja.csound_utils as cs_utils
-from collections import OrderedDict
-import numpy as np
-import csnd6
 import copy
 import random
 import dill
@@ -38,7 +35,7 @@ pitches_to_files = {
 
 
 def cleanup_strings(note, context):
-    note.pfields['inst_file'] = '"' + '/Users/ben/Music/Portfolio/_snd/_gtrs/' + note.pfields['filepitch'] + '.wav' + '"'
+    note.pfields['inst_file'] = '"' + '/Users/ben/Music/Portfolio/snd/_gtrs/' + note.pfields['filepitch'] + '.wav' + '"'
     note.pfields['filepitch'] = '"' + note.pfields['filepitch'] + '"'
 
 
@@ -185,4 +182,4 @@ pulse_l.end_lines = ['i99 0 ' + str(pulse_l.score_dur+10) + ' 5\n']
 print(pulse_l.generate_score_string())
 
 
-cs_utils.play_csound("generic-index.orc", pulse_l, silent=True, args_list=['-o/Users/ben/Music/Portfolio/_csound/index-study-1.wav', '-W'])
+cs_utils.play_csound("generic-index.orc", pulse_l, silent=True, args_list=['-odac3'])
