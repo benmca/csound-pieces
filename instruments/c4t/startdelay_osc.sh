299f0931fca885aaf44768b9c844707cb07f534e
Ben McAllister <bmcallister@xpansiv.com>
2023-05-21 17:00:12 -0700

    new sh

--- instruments/c4t/startdelay_osc.sh
+++ instruments/c4t/startdelay_osc.sh
@@ -1,1 +1,1 @@
-csound -odac1 -iadc1 -b64 -B256 --strset1=10.0.0.205 delay-noclick-osc.csd
+csound -odac2 -iadc2 -b64 -B256 -M1 -W -+rtaudio=coreaudio  --strset1=10.0.0.100 --strset2=true delay-noclick-osc-midi.csd
